000342290ba61e841a65c77c539ead09d841ee65
Sayed2210 <eh600039@gmail.com>
2023-10-10 00:54:10 +0300

    check all labraires

--- src/main.js
+++ src/main.js
@@ -2,5 +2,19 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+//bootstrap
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "normalize.css";
 
-createApp(App).use(store).use(router).mount("#app");
+//fontawesome
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fas, far);
+createApp(App)
+  .component("FIcon", FontAwesomeIcon)
+  .use(store)
+  .use(router)
+  .mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="text-center">elsayed</div>
+    <FIcon :icon="['fas', 'user']" />
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
+<script></script>
